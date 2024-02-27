fdf01611caf4c7bb26fd13af6fdc8048b4c1a676
P3ND4 <106392394+P3ND4@users.noreply.github.com>
2024-02-26 23:17:19 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import uuid
+from datetime import datetime
 
 #generar un id unico
 def gen_id() -> str:
   return str(uuid.uuid1()).split('-')[0]
+
+def sort_by_date(list_of_dict):
+    sorted_dates = sorted(list_of_dict, key=lambda x: datetime.strptime(list(x.keys())[0], '%Y-%m-%d'))
+    return sorted_dates
